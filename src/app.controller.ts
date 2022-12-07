77dd310a1d9e42452fa3f2b55dfcb4bbe75a6fdb
Árpás <arpaspeter0@gmail.com>
2022-12-07 14:06:44 +0100

    jo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,28 @@
-import { Get, Controller, Render } from '@nestjs/common';
+import { Get, Controller, Render, Query, Param } from '@nestjs/common';
 import db from './db';
 
 @Controller()
 export class AppController {
   @Get()
   @Render('list')
-  async listPaintings() {
+  async listPaintings(@Query('year') year = 1990) {
 
-   const[ rows ] = await db.execute('SELECT title FROM paintings');
+   const[ rows ] = await db.execute(
+    'SELECT id , title FROM paintings WHERE year > ?',
+   [ year ]
+   );
 
     return {
       paintings: rows,
     };
+  }
+
+  @Get('paintings/:id')
+ async showPainting(@Param('id') id: number){
+    const [ rows ] = await db.execute(
+      'SELECT title , year , on_display FROM paintings WHERE id = ?',
+      [ id ]
+    );
+    return { painting : rows[0]};
   }
 }
